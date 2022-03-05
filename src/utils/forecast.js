96a9d31cc87710c3c55ef9e00afc7732577b0195
Maksim Sarafimovski <sarafimovski@gmail.com>
2022-03-05 16:53:43 +0100

    Extra data added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,7 @@
 const request = require('postman-request')
 
+
+
 const forecast = (longitude, latitude, callback) => {
     const url = `http://api.weatherstack.com/current?access_key=7b947c85a4645bb67bc25542cdcabc24&query=${latitude},${longitude}&units=m`
 
@@ -9,7 +11,7 @@ const forecast = (longitude, latitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location.', undefined)
         } else {
-            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degress out. It feels like ${body.current.feelslike} degress out.`)
+            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degress out. It feels like ${body.current.feelslike} degress out. The humidity is ${body.current.humidity}%!`)
         }
     })
 }
